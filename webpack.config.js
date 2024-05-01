09609de8ce3a21996b30fd2c312f7382c08599d6
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-01 11:22:35 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
